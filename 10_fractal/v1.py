9ee15e81ca22f2680d1a7c0b5ceea8808462e423
red1239109-cmd <red1239109@gmail.com>
2025-12-12 00:09:15 +0900

    Update v1.py

--- 10_fractal/v1.py
+++ 10_fractal/v1.py
@@ -3,7 +3,13 @@
 # Project: Resonetics Fractal (The Physics of Intelligence)
 # Version: 1.0 (Empirically Proven)
 # Author: red1239109-cmd
+# Copyright (c) 2025 red1239109-cmd
+#
 # License: AGPL-3.0
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # Description:
 #   An AI architecture based on the "Rule of Three".
