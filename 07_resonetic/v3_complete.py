8e1803f19b1e658670e094bfee09aed4ff6003e4
red1239109-cmd <red1239109@gmail.com>
2025-12-14 20:03:16 +0900

    Update v3_complete.py

--- 07_resonetic/v3_complete.py
+++ 07_resonetic/v3_complete.py
@@ -5,28 +5,20 @@
 # Author: red1239109-cmd
 # Copyright (c) 2025 Resonetics Project
 #
-# DUAL LICENSE MODEL:
+# License: AGPL-3.0
 #
-# 1. OPEN SOURCE LICENSE (AGPL-3.0)
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-# 2. COMMERCIAL LICENSE
-#    For organizations that wish to use this software in proprietary products
-#    without open-sourcing their code, a commercial license is available.
-#
-#    Contact: red1239109@gmail.com
-#    Terms: Custom agreement based on organization size and usage
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
 # THIRD-PARTY LICENSES:
 # - NumPy: BSD 3-Clause License
