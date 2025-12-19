6c12c1564e15764bc08fb82231fab367a468b90c
red1239109-cmd <red1239109@gmail.com>
2025-12-19 16:05:55 +0900

    Update __init__.py

--- enterprise/src/__init__.py
+++ enterprise/src/__init__.py
@@ -1,1 +1,10 @@
+import time
 
+def now_ts() -> float:
+    return float(time.time())
+
+def fmt_ts(ts: float) -> str:
+    try:
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ts))
+    except Exception:
+        return str(ts)
