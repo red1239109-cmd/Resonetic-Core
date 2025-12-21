57f9854c6853d34b8a38edd87438557f04a44ab8
red1239109-cmd <red1239109@gmail.com>
2025-12-21 19:30:10 +0900

    Update standalone_dashboard.py

--- enterprise/standalone_dashboard.py
+++ enterprise/standalone_dashboard.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# SPDX-License-Identifier: AGPL-3.0-or-later
+# SPDX-License-Identifier: Apache-2.0
 # Copyright (C) 2025 red1239109-cmd
 # ==============================================================================
 # File: standalone_dashboard.py_v1.2.py
