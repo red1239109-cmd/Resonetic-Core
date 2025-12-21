90248262b9a5cb35fa04377b9ca8a8514200c1f6
red1239109-cmd <red1239109@gmail.com>
2025-12-21 19:46:10 +0900

    Update resonetics_via_negativa_v1_2_weighted.py

--- researchnote/resonetics_via_negativa_v1_2_weighted.py
+++ researchnote/resonetics_via_negativa_v1_2_weighted.py
@@ -1,24 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2025 red1239109-cmd
 # ==============================================================================
 # File: resonetics_via_negativa_v1_2_weighted.py
 # Project: Resonetics – Via Negativa
 # Version: 1.2 (Confidence-Weighted Paradox Engine)
-# Author: red1239109-cmd
-# Copyright (c) 2025 Resonetics Project
-#
-# License: GNU Affero General Public License v3.0 (AGPL-3.0)
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
 # ==============================================================================
 
 from dataclasses import dataclass
